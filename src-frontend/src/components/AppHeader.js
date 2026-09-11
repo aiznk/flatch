1b0b6d36666c7a4e4a9a4b7b19172cf0f295803a
aiznk <aiznk@users.noreply.github.com>
2026-09-11 13:45:02 +0900

    AppHeader design

--- src-frontend/src/components/AppHeader.js
+++ src-frontend/src/components/AppHeader.js
@@ -1,19 +1,31 @@
 import * as nue from '../nue/nue.js'
 import * as i18n from '../i18n.js'
 
+class AppTitle extends nue.H1 {
+	constructor () {
+		super('', { class: 'app-title' })
+
+		this.link = new nue.Link(i18n.home(), { path: '/' }, '/', { class: 'link' })
+		this.link.setText(FLATCH.APP_TITLE)
+		this.add(this.link)
+	}
+}
+
 export default class AppHeader extends nue.Div {
 	constructor (mainModel) {
 		super({ class: 'app-header' })
 		this.mainModel = mainModel
 
-		this.appTitle = new nue.H1('', { class: 'app-title' })
-		this.appTitle.setText(FLATCH.APP_TITLE)
+		this.appTitle = new AppTitle()
 		this.add(this.appTitle)
 
-		this.home = new nue.Link(i18n.home(), { path: '/home' }, '/?path=/home', { class: 'link' })
-		this.add(this.home)
+		this.mainMenu = new nue.Div({ class: 'main-menu' })
+		this.add(this.mainMenu)
 
-		this.boards = new nue.Link(i18n.boards(), { path: `/${FLATCH.BOARDS_DIR_NAME}` }, `/?path=/${FLATCH.BOARDS_DIR_NAME}`, { class: 'link' })
-		this.add(this.boards)
+		this.home = new nue.Link(i18n.home(), { path: '/home' }, '/?path=/home', { class: 'menu-item link' })
+		this.mainMenu.add(this.home)
+
+		this.boards = new nue.Link(i18n.boards(), { path: `/${FLATCH.BOARDS_DIR_NAME}` }, `/?path=/${FLATCH.BOARDS_DIR_NAME}`, { class: 'menu-item link' })
+		this.mainMenu.add(this.boards)
 	}
 }
