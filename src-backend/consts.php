05ec8ebeca9b2a44ddd000de36f9e11ee60f1584
aiznk <aiznk@users.noreply.github.com>
2026-09-12 12:28:59 +0900

    refactoring

--- src-backend/consts.php
+++ src-backend/consts.php
@@ -1,20 +1,50 @@
 <?php
+// デフォルトのアプリのタイトル
 define('DEF_APP_TITLE', 'Flatch');
+
+// アプリのデータを配置するフォルダ
+// データを非公開にしたい場合はこの定数を任意のフォルダに変えてください。
 define('DATA_DIR', './data');
+
+// POST（投稿）の時間制限（秒）
 define('MAX_LAST_POST_DIF', 3); // x seconds
+
+// アプリの構成情報が書かれているファイル
 define('CONFIG_PATH', DATA_DIR .'/config.json');
+
+// 板リストの情報が書かれているファイル
 define('BOARDS_PATH', DATA_DIR .'/boards.json');
+
+// カテゴリ情報が書かれているファイル
 define('CATEGORIES_PATH', DATA_DIR .'/categories.json');
+
+// スレッドのIDをカウントするファイル
 define('THREAD_ID_PATH', DATA_DIR .'/thread_id.dat');
+
+// 静的ファイルを置くフォルダ
 define('STATIC_DIR', './static');
 define('INDEX_JS_PATH', STATIC_DIR .'/index.js');
 define('STYLE_CSS_PATH', STATIC_DIR .'/style.css');
+
+// 板フォルダを配置する親フォルダ名
 define('BOARDS_DIR_NAME', 'boards');
+
+// 板フォルダを配置する親フォルダ
 define('BOARDS_DIR', DATA_DIR . '/'. BOARDS_DIR_NAME);
-define('THREADS_DIR_NAME', 'threads');
-define('SUBJECTS_FILE_NAME', 'subjects.dat');
+
+// 板の設定ファイル名
 define('BOARD_SETTING_FILE_NAME', 'setting.json');
+
+// スレッドデータを配置する親フォルダ名
+define('THREADS_DIR_NAME', 'threads');
+
+// 板の表示するスレッドの一覧
+define('SUBJECTS_FILE_NAME', 'subjects.dat');
+
+// datファイルの行の区切り文字
 define('DAT_LINE_SEP', '<>');
+
+// datファイルの拡張子
 define('DAT_FILE_EXT', '.dat');
 
 // スレッドに投稿できるレコードの上限数
