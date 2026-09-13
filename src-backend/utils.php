e4f266bc800ee195ea09a2f8de55104182c70253
aiznk <aiznk@users.noreply.github.com>
2026-09-13 13:26:03 +0900

    remove touch_dirs()

--- src-backend/utils.php
+++ src-backend/utils.php
@@ -92,13 +92,6 @@ function safe ($val) {
 	return htmlspecialchars($val);
 }
 
-function touch_dirs ($dir) {
-	if (!file_exists($dir)) {
-		return mkdir($dir, 0766, true);
-	}
-	return true;
-}
-
 function check_path ($path) {
 	if (strpos($path, '..') !== false) {
 		die("found '..' in path");
